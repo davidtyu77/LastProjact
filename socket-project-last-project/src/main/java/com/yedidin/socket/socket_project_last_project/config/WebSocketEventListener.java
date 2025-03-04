a87e654d1a61f493ebc00817f728e7b32786accd
Saar Yosov <saar.yosov@gmail.com>
2025-03-04 12:29:57 +0200

    initial

---
+++ socket-project-last-project/src/main/java/com/yedidin/socket/socket_project_last_project/config/WebSocketEventListener.java
@@ -0,0 +1,26 @@
+package com.yedidin.socket.socket_project_last_project.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(
+            SessionDisconnectEvent event
+    ){
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if(username != null){
+
+        }
+    }
+
+}
